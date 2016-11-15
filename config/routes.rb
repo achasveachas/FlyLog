053ae01c5e328f54f4b348d11ed3cd605164b547
Yechiel Kalmenson <chilikkal@gmail.com>
2016-11-15 04:39:59 +0000

    created pilot controller and pilot form

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
-  get 'index/home'
-
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'index#home'
   get '/signin' => 'sessions#new'
   post '/signin' => 'sessions#create'
   post '/logout' => 'sessions#destroy'
+  resources :pilots
 end
