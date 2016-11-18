325fd4f70b79610e4c313a2314ab09d0ca138e8d
Yechiel Kalmenson <chilikkal@gmail.com>
2016-11-18 02:48:31 +0000

    added total_flight_time method to LogBook

--- app/models/log_book.rb
+++ app/models/log_book.rb
@@ -1,4 +1,15 @@
+require 'pry'
 class LogBook < ApplicationRecord
   belongs_to :pilot
   has_many :flights
+
+  def total_flight_time
+    minutes = self.flights.size > 0 ? self.flights.sum(:duration) : 0
+    hours = 0
+    until minutes < 60
+      minutes -= 60
+      hours += 1
+    end
+    "#{hours}:#{minutes.to_s.rjust(2, "0")}"
+  end
 end
