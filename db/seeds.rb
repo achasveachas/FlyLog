2f4ba99e3452132c85c7161392a07c8d6631fe48
Yechiel Kalmenson <chilikkal@gmail.com>
2016-11-24 06:46:39 +0000

    created seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,3 +15,23 @@ Airplane.create(make: "Cessna", model: "C172")
 Airplane.create(make: "Piper", model: "Cherokee")
 Airplane.create(make: "Boing", model: "747-8")
 Airplane.create(make: "Airbus", model: "A380")
+
+
+20.times do
+  pilot = Pilot.new(name: Faker::Name.name, email: Faker::Internet.free_email, age: rand(16..99), password: "testtest", password_confirmation: "testtest")
+  3.times do
+    pilot.ratings << Rating.order("RANDOM()").first
+  end
+  pilot.save
+end
+
+Pilot.all.each do |pilot|
+  5.times do
+    flight = pilot.flights.new(date: Date.today, duration: rand(45..450), notes: Faker::Lorem.sentences(3).join(" "), instructor: Faker::Name.name)
+    flight.airplanes << Airplane.order("RANDOM()").first
+    flight.flight_airplanes.first.tail_number = "N#{Faker::Number.number(6)}"
+    flight.save
+  end
+end
+
+end
